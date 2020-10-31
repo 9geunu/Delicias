09418713ef57ad0e8fd4914015572df69c8a56fb
walter <dev.ggw@gmail.com>
2020-11-01 01:06:32 +0900

    Add naver map components in MapFragment

--- app/src/main/java/com/example/delicias/ui/MapFragment.kt
+++ app/src/main/java/com/example/delicias/ui/MapFragment.kt
@@ -1,6 +1,74 @@
 package com.example.delicias.ui
 
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.delicias.R
+import com.example.delicias.databinding.FragmentMapBinding
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.geometry.LatLngBounds
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.util.FusedLocationSource
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+    lateinit var binding: FragmentMapBinding
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_map, container, false)
+        val fm = childFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map) as com.naver.maps.map.MapFragment?
+            ?: com.naver.maps.map.MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map, it).commit()
+            }
+
+        locationSource =
+            FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+
+        mapFragment.getMapAsync(this)
+
+        return binding.root
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
+                grantResults)) {
+            if (!locationSource.isActivated) { // 권한 거부됨
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onMapReady(naverMap: NaverMap) {
+        Log.d("delishas", "onMapReady!")
+        this.naverMap = naverMap
+        this.naverMap.locationSource = locationSource
+        this.naverMap.mapType = NaverMap.MapType.Basic
+        this.naverMap.minZoom = 7.0
+        this.naverMap.maxZoom = 19.0
+        this.naverMap.isIndoorEnabled = true
+        this.naverMap.extent = LatLngBounds(LatLng(31.43, 122.37), LatLng(44.35, 132.0))
+        val uiSettings = this.naverMap.uiSettings
+        uiSettings.isLocationButtonEnabled = true
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
+    }
+
 }
