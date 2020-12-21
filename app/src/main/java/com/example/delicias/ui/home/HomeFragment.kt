75eb9d4f14b24436138df2d6a3b3f73c070524f1
walter <dev.ggw@gmail.com>
2020-12-21 23:50:14 +0900

    Add 'sort by location' function in HomeFragment

--- app/src/main/java/com/example/delicias/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/delicias/ui/home/HomeFragment.kt
@@ -1,12 +1,24 @@
 package com.example.delicias.ui.home
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context.LOCATION_SERVICE
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.SearchView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
@@ -21,13 +33,16 @@ import com.example.delicias.data.repository.RestaurantDataRepository
 import com.example.delicias.data.repository.datasource.LocalRestaurantDataStore
 import com.example.delicias.databinding.FragmentHomeBinding
 import com.example.delicias.domain.Date
+import com.example.delicias.domain.Restaurant
 import com.example.delicias.domain.RestaurantMinimal
 import com.example.delicias.ui.DateAdapter
 import com.example.delicias.ui.MealTimeAdapter
 import com.example.delicias.ui.RestaurantMinimalAdapter
 import com.example.delicias.util.Util
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
+import java.util.*
+
 
 class HomeFragment : Fragment(), SearchView.OnQueryTextListener {
     lateinit var homeViewModel: HomeViewModel
@@ -38,6 +53,14 @@ class HomeFragment : Fragment(), SearchView.OnQueryTextListener {
     lateinit var lifecycleOwner: LifecycleOwner
     var currentMealTime = MealTime.BREAKFAST
     lateinit var restaurantDataRepository :RestaurantDataRepository
+    val scope = CoroutineScope(Dispatchers.Default)
+
+    private val GPS_ENABLE_REQUEST_CODE = 2001
+    private val PERMISSIONS_REQUEST_CODE = 100
+    var REQUIRED_PERMISSIONS = arrayOf<String>(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
 
     enum class MealTime{
         BREAKFAST,
@@ -99,8 +122,63 @@ class HomeFragment : Fragment(), SearchView.OnQueryTextListener {
             ) {
                 when(position){
                     0 -> {
+                        when (currentMealTime) {
+                            MealTime.BREAKFAST -> {
+                                scope.launch {
+                                    val breakfastList =
+                                        restaurantDataRepository.getAllBreakfast().first()
+                                    withContext(Dispatchers.Main) {
+                                        sortByName(breakfastList)
+                                    }
+                                }
+                            }
+                            MealTime.LUNCH -> {
+                                scope.launch {
+                                    val lunchList = restaurantDataRepository.getAllLunch().first()
+                                    withContext(Dispatchers.Main) {
+                                        sortByName(lunchList)
+                                    }
+                                }
+                            }
+                            MealTime.DINNER -> {
+                                scope.launch {
+                                    val dinnerList = restaurantDataRepository.getAllDinner().first()
+                                    withContext(Dispatchers.Main) {
+                                        sortByName(dinnerList)
+                                    }
+                                }
+                            }
+                        }
                     }
                     1 -> {
+                        Log.d("delitag", Util.getLocation(requireContext()).toString())
+                        when (currentMealTime) {
+                            MealTime.BREAKFAST -> {
+                                scope.launch {
+                                    val breakfastList =
+                                        restaurantDataRepository.getAllBreakfast().first()
+                                    withContext(Dispatchers.Main){
+                                        sortByLocation(breakfastList)
+                                    }
+                                }
+                            }
+                            MealTime.LUNCH -> {
+                                scope.launch {
+                                    val lunchList = restaurantDataRepository.getAllLunch().first()
+                                    withContext(Dispatchers.Main){
+                                        sortByLocation(lunchList)
+                                    }
+                                }
+                            }
+                            MealTime.DINNER -> {
+                                scope.launch {
+                                    val dinnerList = restaurantDataRepository.getAllDinner().first()
+                                    withContext(Dispatchers.Main){
+                                        sortByLocation(dinnerList)
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -110,9 +188,22 @@ class HomeFragment : Fragment(), SearchView.OnQueryTextListener {
             }
         }
 
-        homeViewModel.getAllRestaurantMinimal().observe(lifecycleOwner, androidx.lifecycle.Observer {
-            restaurantMinimalAdapter.submitList(it)
-        })
+        runBlocking {
+            val breakfastList = restaurantDataRepository.getAllBreakfast().first()
+            restaurantDataRepository.updateRestaurantMinimals(breakfastList)
+        }
+
+        homeViewModel.getAllRestaurantMinimal().observe(
+            lifecycleOwner,
+            androidx.lifecycle.Observer {
+                restaurantMinimalAdapter.submitList(it)
+            })
+
+        if (!checkLocationServicesStatus()) {
+            showDialogForLocationServiceSetting()
+        } else {
+            checkRunTimePermission()
+        }
 
         return binding.root
     }
@@ -121,34 +212,67 @@ class HomeFragment : Fragment(), SearchView.OnQueryTextListener {
         override fun onPageSelected(position: Int) {
             super.onPageSelected(position)
              when (position) {
-                0 -> {
-                    currentMealTime = MealTime.BREAKFAST
-                    runBlocking {
-                        val breakfastList = restaurantDataRepository.getAllBreakfast().first()
-                        restaurantDataRepository.updateRestaurantMinimals(breakfastList)
-                    }
-                }
-                1 -> {
-                    currentMealTime = MealTime.LUNCH
-                    runBlocking {
-                        val lunchList = restaurantDataRepository.getAllLunch().first()
-                        restaurantDataRepository.updateRestaurantMinimals(lunchList)
-                    }
-                }
-                2 -> {
-                    currentMealTime = MealTime.DINNER
-                    runBlocking {
-                        val dinnerList = restaurantDataRepository.getAllDinner().first()
-                        restaurantDataRepository.updateRestaurantMinimals(dinnerList)
-                    }
-                }
+                 0 -> {
+                     currentMealTime = MealTime.BREAKFAST
+                     scope.launch {
+                         val breakfastList = restaurantDataRepository.getAllBreakfast().first()
+                         when (binding.spnListingOrder1.selectedItemPosition) {
+                             0 -> {
+                                 withContext(Dispatchers.Main) {
+                                     sortByName(breakfastList)
+                                 }
+                             }
+                             1 -> {
+                                 withContext(Dispatchers.Main) {
+                                     sortByLocation(breakfastList)
+                                 }
+                             }
+                         }
+                     }
+                 }
+                 1 -> {
+                     currentMealTime = MealTime.LUNCH
+                     scope.launch {
+                         val lunchList = restaurantDataRepository.getAllLunch().first()
+                         when (binding.spnListingOrder1.selectedItemPosition) {
+                             0 -> {
+                                 withContext(Dispatchers.Main) {
+                                     sortByName(lunchList)
+                                 }
+                             }
+                             1 -> {
+                                 withContext(Dispatchers.Main) {
+                                     sortByLocation(lunchList)
+                                 }
+                             }
+                         }
+                     }
+                 }
+                 2 -> {
+                     currentMealTime = MealTime.DINNER
+                     scope.launch {
+                         val dinnerList = restaurantDataRepository.getAllDinner().first()
+                         when (binding.spnListingOrder1.selectedItemPosition) {
+                             0 -> {
+                                 withContext(Dispatchers.Main) {
+                                     sortByName(dinnerList)
+                                 }
+                             }
+                             1 -> {
+                                 withContext(Dispatchers.Main) {
+                                     sortByLocation(dinnerList)
+                                 }
+                             }
+                         }
+                     }
+                 }
                 else -> error("no such position: $position")
             }
         }
     }
 
     fun insertAll(){
-        runBlocking {
+        scope.launch {
             homeViewModel.getRestaurantsFromRemote()
         }
     }
@@ -172,23 +296,194 @@ class HomeFragment : Fragment(), SearchView.OnQueryTextListener {
 
         when (currentMealTime){
             MealTime.BREAKFAST -> {
-                runBlocking {
-                    val searchedBreakfastList = restaurantDataRepository.searchForBreakfast(queryText).first()
+                scope.launch {
+                    val searchedBreakfastList = restaurantDataRepository.searchForBreakfast(
+                        queryText
+                    ).first()
                     restaurantDataRepository.updateRestaurantMinimals(searchedBreakfastList)
                 }
             }
             MealTime.LUNCH -> {
-                runBlocking {
-                    val searchedLunchList = restaurantDataRepository.searchForLunch(queryText).first()
+                scope.launch {
+                    val searchedLunchList =
+                        restaurantDataRepository.searchForLunch(queryText).first()
                     restaurantDataRepository.updateRestaurantMinimals(searchedLunchList)
                 }
             }
             MealTime.DINNER -> {
-                runBlocking {
-                    val searchedDinnerList = restaurantDataRepository.searchForDinner(queryText).first()
+                scope.launch {
+                    val searchedDinnerList =
+                        restaurantDataRepository.searchForDinner(queryText).first()
                     restaurantDataRepository.updateRestaurantMinimals(searchedDinnerList)
                 }
             }
         }
+    }
+
+    private fun sortByName(restaurantMinimals: List<RestaurantMinimal>) {
+        Util.sortByName(restaurantMinimals)
+        restaurantMinimalAdapter.submitList(restaurantMinimals)
+    }
+
+    private fun sortByLocation(restaurantMinimals: List<RestaurantMinimal>) {
+        Collections.sort(restaurantMinimals, kotlin.Comparator { left, right ->
+            val leftRestaurant: Restaurant
+            val rightRestaurant: Restaurant
+            val currentLocation = Util.getLocation(requireContext())
+
+            runBlocking {
+                leftRestaurant = restaurantDataRepository.getRestaurantById(left.id).first()
+                rightRestaurant = restaurantDataRepository.getRestaurantById(right.id).first()
+            }
+
+            if (Util.calculateRestaurantDistance
+                    (currentLocation, leftRestaurant.latitude, leftRestaurant.longitude) >
+                Util.calculateRestaurantDistance
+                    (currentLocation, rightRestaurant.latitude, rightRestaurant.longitude))
+                return@Comparator 1
+            else if (Util.calculateRestaurantDistance
+                    (currentLocation, leftRestaurant.latitude, leftRestaurant.longitude) <
+                Util.calculateRestaurantDistance
+                    (currentLocation, rightRestaurant.latitude, rightRestaurant.longitude))
+                return@Comparator -1
+            else return@Comparator 0
+        })
+
+        restaurantMinimalAdapter.submitList(restaurantMinimals)
+    }
+
+    override fun onRequestPermissionsResult(
+        permsRequestCode: Int,
+        permissions: Array<String>,
+        grandResults: IntArray
+    ) {
+        if (permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.size == REQUIRED_PERMISSIONS.size) {
+
+            // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
+            var check_result = true
+
+            // 모든 퍼미션을 허용했는지 체크합니다.
+            for (result in grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false
+                    break
+                }
+            }
+            if (check_result) {
+                //위치 값을 가져올 수 있음
+            } else {
+                // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있습니다.
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        this.requireActivity(),
+                        REQUIRED_PERMISSIONS[0]
+                    )
+                    || ActivityCompat.shouldShowRequestPermissionRationale(
+                        this.requireActivity(),
+                        REQUIRED_PERMISSIONS[1]
+                    )
+                ) {
+                    Toast.makeText(
+                        this.requireContext(),
+                        "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Toast.makeText(
+                        this.requireContext(),
+                        "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            GPS_ENABLE_REQUEST_CODE ->
+                //사용자가 GPS 활성 시켰는지 검사
+                if (checkLocationServicesStatus()) {
+                    Log.d("delitag", "onActivityResult : GPS 활성화 되있음")
+                    checkRunTimePermission()
+                    return
+                }
+        }
+    }
+
+    private fun checkRunTimePermission() {
+        //런타임 퍼미션 처리
+        // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
+        val hasFineLocationPermission = ContextCompat.checkSelfPermission(
+            this.requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(
+            this.requireContext(),
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+            hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED
+        ) {
+
+            // 2. 이미 퍼미션을 가지고 있다면
+            // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
+
+
+            // 3.  위치 값을 가져올 수 있음
+        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
+
+            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this.requireActivity(),
+                    REQUIRED_PERMISSIONS[0]
+                )
+            ) {
+                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
+                Toast.makeText(
+                    this.requireContext(),
+                    "이 앱을 실행하려면 위치 접근 권한이 필요합니다.",
+                    Toast.LENGTH_LONG
+                ).show()
+                // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(
+                    this.requireActivity(), REQUIRED_PERMISSIONS,
+                    PERMISSIONS_REQUEST_CODE
+                )
+            } else {
+                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
+                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(
+                    this.requireActivity(), REQUIRED_PERMISSIONS,
+                    PERMISSIONS_REQUEST_CODE
+                )
+            }
+        }
+    }
+
+    private fun showDialogForLocationServiceSetting() {
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this.requireContext())
+        builder.setTitle("위치 서비스 비활성화")
+        builder.setMessage(
+            """
+            앱을 사용하기 위해서는 위치 서비스가 필요합니다.
+            위치 설정을 수정하실래요?
+            """.trimIndent()
+        )
+        builder.setCancelable(true)
+        builder.setPositiveButton("설정") { dialog, id ->
+            val callGPSSettingIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE)
+        }
+        builder.setNegativeButton("취소") { dialog, id ->
+            dialog.cancel()
+        }
+        builder.create().show()
+    }
+
+    private fun checkLocationServicesStatus(): Boolean {
+        val locationManager = requireContext().getSystemService(LOCATION_SERVICE) as LocationManager?
+        return (locationManager!!.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
     }
 }
--- app/src/main/java/com/example/delicias/util/Util.kt
+++ app/src/main/java/com/example/delicias/util/Util.kt
@@ -1,13 +1,43 @@
 package com.example.delicias.util
 
+import android.Manifest
+import android.content.Context
+import android.content.Context.LOCATION_SERVICE
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import android.util.Log
+import androidx.core.content.ContextCompat
 import com.example.delicias.domain.Date
+import com.example.delicias.domain.RestaurantMinimal
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import java.lang.Math.pow
 import java.util.*
 import kotlin.collections.ArrayList
+import kotlin.math.sqrt
+
 
 class Util {
     companion object{
+        private const val MIN_DISTANCE_CHANGE_FOR_UPDATES: Long = 10
+        private const val MIN_TIME_BW_UPDATES = (1000 * 60 * 1).toLong()
+        private var locationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location?) {
+            }
+
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+            }
+
+            override fun onProviderEnabled(provider: String?) {
+            }
+
+            override fun onProviderDisabled(provider: String?) {
+            }
+        }
+
         fun getDates(): Flow<ArrayList<Date>> = flow {
             var dateList = ArrayList<Date>()
             var calender = Calendar.getInstance(Locale.KOREA)
@@ -15,11 +45,13 @@ class Util {
             for (i in 0..6) {
                 var temp =  if (i == 0) 0 else 1
 
-                calender.add(Calendar.DATE,  temp)
+                calender.add(Calendar.DATE, temp)
 
                 dateList.add(
-                    Date(getKorDayOfWeek(calender.get(Calendar.DAY_OF_WEEK)),
-                        getDateString(calender.get(Calendar.DATE)), i == 0)
+                    Date(
+                        getKorDayOfWeek(calender.get(Calendar.DAY_OF_WEEK)),
+                        getDateString(calender.get(Calendar.DATE)), i == 0
+                    )
                 )
             }
 
@@ -50,6 +82,76 @@ class Util {
             }
 
             return dateString
+        }
+
+        fun sortByName(list: List<RestaurantMinimal>){
+            Collections.sort(list, kotlin.Comparator { left, right ->
+                if (left.name.first() > right.name.first())
+                    return@Comparator 1
+                else if (left.name.first() < right.name.first())
+                    return@Comparator -1
+                else return@Comparator 0
+            })
+        }
+
+        fun getLocation(context: Context): Location? {
+            var locationManager: LocationManager?
+            var location: Location? = null
+            try {
+                locationManager = context.getSystemService(LOCATION_SERVICE) as LocationManager?
+                val isGPSEnabled: Boolean =
+                    locationManager?.isProviderEnabled(LocationManager.GPS_PROVIDER) ?: false
+                val isNetworkEnabled: Boolean =
+                    locationManager?.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ?: false
+                if (!isGPSEnabled && !isNetworkEnabled) {
+                } else {
+                    val hasFineLocationPermission = ContextCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    )
+                    val hasCoarseLocationPermission = ContextCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                    )
+                    if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+                        hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED
+                    ) {
+                    } else return null
+                    if (isNetworkEnabled) {
+                        locationManager?.requestLocationUpdates(
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_BW_UPDATES,
+                            MIN_DISTANCE_CHANGE_FOR_UPDATES.toFloat(),
+                            locationListener
+                        )
+                        if (locationManager != null) {
+                            location =
+                                locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+                        }
+                    }
+                    if (isGPSEnabled) {
+                        if (location == null) {
+                            locationManager?.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES.toFloat(),
+                                locationListener
+                            )
+                            if (locationManager != null) {
+                                location =
+                                    locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                            }
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                Log.d("delitag", e.toString())
+            }
+            return location
+        }
+
+        fun calculateRestaurantDistance(currentLocation: Location?, restaurantLat: Double, restaurantLng: Double): Double {
+            return sqrt(pow(currentLocation?.latitude?.minus(restaurantLat)!!, 2.0) + pow(currentLocation.longitude.minus(restaurantLng), 2.0))
         }
     }
 }
