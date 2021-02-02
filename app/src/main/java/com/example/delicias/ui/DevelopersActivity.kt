a170ef8ab9172a186ad415ad877d1f5abcf2d927
walter <dev.ggw@gmail.com>
2021-02-02 23:13:26 +0900

    Extract back button title layout

--- app/src/main/java/com/example/delicias/ui/DevelopersActivity.kt
+++ app/src/main/java/com/example/delicias/ui/DevelopersActivity.kt
@@ -2,17 +2,31 @@ package com.example.delicias.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingUtil
 import com.example.delicias.R
 import com.example.delicias.databinding.ActivityDevelopersBinding
 import kotlinx.android.synthetic.main.activity_developers.*
 
 class DevelopersActivity : AppCompatActivity() {
     lateinit var binding: ActivityDevelopersBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_developers)
+    private lateinit var ivBackButton: ImageView
 
-        iv_back_button.setOnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
+
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_developers)
+
+        val backButtonTitle: View = binding.backButtonTitle
+
+        ivBackButton = (backButtonTitle as ConstraintLayout).getViewById(R.id.iv_back_button) as ImageView
+
+        ivBackButton.setOnClickListener {
             onBackPressed()
         }
     }
